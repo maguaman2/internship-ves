5976dc7a811a539e2aea7638c163b002525243d6
eaochoa <148830588+enriqueooo@users.noreply.github.com>
2024-12-06 09:54:25 -0500

    cre las carpetas

--- src/main/kotlin/ec/edu/sudamericano/internship_ves/repository/EvaluationRepository.kt
+++ src/main/kotlin/ec/edu/sudamericano/internship_ves/repository/EvaluationRepository.kt
@@ -1,4 +1,8 @@
 package ec.edu.sudamericano.internship_ves.repository
 
-interface EvaluationRepository {
-}
+import ec.edu.sudamericano.internship_ves.entity.Evaluation
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface EvaluationRepository : JpaRepository<Evaluation, Long>
--- src/main/kotlin/ec/edu/sudamericano/internship_ves/service/EvaluationService.kt
+++ src/main/kotlin/ec/edu/sudamericano/internship_ves/service/EvaluationService.kt
@@ -1,4 +1,33 @@
 package ec.edu.sudamericano.internship_ves.service
 
-class EvaluationService {
+import ec.edu.sudamericano.internship_ves.entity.Evaluation
+import ec.edu.sudamericano.internship_ves.repository.EvaluationRepository
+import org.springframework.stereotype.Service
+import java.util.Optional
+
+@Service
+class EvaluationService(private val evaluationRepository: EvaluationRepository) {
+
+    fun getAllEvaluations(): List<Evaluation> = evaluationRepository.findAll()
+
+    fun getEvaluationById(id: Long): Optional<Evaluation> = evaluationRepository.findById(id)
+
+    fun createEvaluation(evaluation: Evaluation): Evaluation = evaluationRepository.save(evaluation)
+
+    fun updateEvaluation(id: Long, evaluation: Evaluation): Optional<Evaluation> {
+        return if (evaluationRepository.existsById(id)) {
+            Optional.of(evaluationRepository.save(evaluation.copy(id = id)))
+        } else {
+            Optional.empty()
+        }
+    }
+
+    fun deleteEvaluation(id: Long): Boolean {
+        return if (evaluationRepository.existsById(id)) {
+            evaluationRepository.deleteById(id)
+            true
+        } else {
+            false
+        }
+    }
 }
