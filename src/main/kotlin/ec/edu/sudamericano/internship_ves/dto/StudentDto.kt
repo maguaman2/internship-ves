31001aaf0747052ee2b6f506b7ec7ef56c3601a4
a <146010164+arielsuper46@users.noreply.github.com>
2024-12-12 14:38:05 -0500

    Se actualizo en dto

--- src/main/kotlin/ec/edu/sudamericano/internship_ves/dto/StudentDto.kt
+++ src/main/kotlin/ec/edu/sudamericano/internship_ves/dto/StudentDto.kt
@@ -1,11 +1,13 @@
 package ec.edu.sudamericano.internship_ves.dto
 
+
+
 class StudentDto {
-    var id: Long? = null
-    var firstName: String? = null
-    var lastName: String? = null
-    var nui: String? = null
-    var photoUrl: String? = null
-    var email: String? = null
-    var careerId: Long? = null
+    val id: Long? = null
+    val firstName: String? = null
+    val lastName: String? = null
+    val nui: String? = null
+    val photoUrl: String? = null
+    val email: String? = null
+    val careerId: Long? = null
 }
